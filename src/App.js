85c4fc01c8707719e5ed981c9232a1d55caef5d2
nithin3119@gmail.com <nithin3119@gmail.com>
2023-04-12 11:05:32 +0530

    React To

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, {useEffect, useState , useRef} from 'react';
 import './App.css';
 
+//import components
+import {Form} from './components/Form';
+
+import { TodoList } from './components/TodoList';
+
 function App() {
+  const LOCAL_STORAGE_KEY = "todos";
+  const[inputText,setInputText] = useState("");
+  const [todos,setTodos] = useState( JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos ,setFilteredTodos] = useState([])
+  const inputEl  = useRef("");
+  
+ 
+
+  const filterHandler = () =>{
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+         break;  
+    
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  }
+
+ 
+
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+    filterHandler(); 
+  },[todos,status]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+      <h1>Todo List  </h1>
+    </header>
+    < Form setInputText ={setInputText} inputText = {inputText} todos = {todos} setTodos ={setTodos} setStatus={setStatus} inputEl ={inputEl}/>
+    < TodoList setTodos ={setTodos} todos = {todos} filteredTodos ={filteredTodos}/>
     </div>
   );
 }
 
 export default App;
+ 
